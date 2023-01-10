ea67312f977097833532afea0efb326153a72766
Muk007 <38641966+Muk007@users.noreply.github.com>
2023-01-10 11:14:35 +0530

    Update mongo-install.py

--- mongo-install.py
+++ mongo-install.py
@@ -28,19 +28,20 @@ def mongo_config(mongo_home):
     cmd = "tar -xvf "+mongo_home+"/mongodb-6.0.3.tgz -C "+mongo_home+" && mv "+mongo_home+"/mongodb-linux-x86_64-ubuntu2004-6.0.3 "+mongo_home+"/mongodb && sudo cp -R "+mongo_home+"/mongodb/bin/mongod /usr/local/bin/mongod && sudo chmod +x /usr/local/bin/mongod"
     os.system(cmd)
     logging.info("Mongo binary configured.")
-
-def install_mongo_client(client):
+    
+def mongo_client_and_mongoConfig_setup(client, mongo_home):
     cmd = "sudo apt-get install -y "+client
     os.system(cmd)
-
-def start_script(data_path, service_name):
+    shutil.copyfile("/home/ubuntu/mongo-setup/mongodb.conf", mongo_home+"/mongodb.conf")
+    
+def start_script(data_path, service_name, mongo_home):
     try:
         shutil.rmtree(data_path)
     except:
         pass
     os.makedirs(data_path)
     with open("/etc/systemd/system/mongodb.service", "w") as file:
-        lines = ["[Unit]\n", "Description=MongoDB Service\n", "Documentation=https://www.mongodb.com/\n\n", "[Service]\n","ExecStart=mongod --dbpath "+data_path+"\n\n", "[Install]\n", "WantedBy=multi-user.target\n", "Alias=mongodb.service\n"]
+        lines = ["[Unit]\n", "Description=MongoDB Service\n", "Documentation=https://www.mongodb.com/\n\n", "[Service]\n","ExecStart=mongod --config "+mongo_home+"/mongodb.conf --dbpath "+data_path+"\n\n", "[Install]\n", "WantedBy=multi-user.target\n", "Alias=mongodb.service\n"]
         file.writelines(lines)
         file.close()
     cmd = "sudo systemctl start "+service_name
@@ -48,12 +49,13 @@ def start_script(data_path, service_name):
     logging.info("Mongodb started...")
     
 config_data = {}
+
 try:
     config_data = load_config(sys.argv[1])
     download_binary(config_data['url'], config_data['mongo_home']) 
     mongo_config(config_data['mongo_home'])
-    install_mongo_client(config_data['client_name'])
-    start_script(config_data['data_path'], config_data['service_name'])
-
+    mongo_client_and_mongoConfig_setup(config_data['client_name'], config_data['mongo_home'])
+    start_script(config_data['data_path'], config_data['service_name'], config_data['mongo_home'])
+    
 except Exception as exp:
     logging.exception("Mongo installation failed.")
